33d9aa78ca6868c6f66ea124f8eebc7266a68257
Unknown <clarika.91@gmail.com>
2019-08-09 15:52:48 +0200

    Update main comments

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,21 +3,14 @@ import { Path } from './Modele/path';
 const fs = require('fs');
 let frequences = require("./Parseur/frequences");
 
-//Remarques :  Lancement du main 
-/**
- * cd DLK/app
- * node dist/main.js
- * console.log("diraname: "+__dirname); -> C:\Users\claire.ricard\Desktop\DLK\app\dist
- *   console.log("process: "+process.cwd()); -> C:\Users\claire.ricard\Desktop\DLK\app
- * => Solution, repertoire de base = repertoire où est situé le fichier main 
- * => variables dir_path définies au lancement du main
- */
-
-
 console.log("debut main");
-
+// Init the server user path :
 if (!fs.existsSync(Path.userPath)) {
     fs.mkdirSync(Path.userPath);
 }
+
+// Create frequence files :
 frequences.GbdiToFreq(Path.STPVFilePath);
+
+// Start application :
 new Formulaire();
