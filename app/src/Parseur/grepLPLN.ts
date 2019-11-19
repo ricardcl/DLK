8b70d01c5ccb171d1fc4cad432efc3db0f5c3120
Unknown <clarika.91@gmail.com>
2019-11-19 10:22:39 +0100

    recuperation de l'information ETATDL dans le fichier LPLN

--- app/src/Parseur/grepLPLN.ts
+++ app/src/Parseur/grepLPLN.ts
@@ -116,7 +116,7 @@ export class GrepLPLN {
               mylogCpdlc = this.readLine.fgets(r);
               if (mylogCpdlc === false) { break; }
 
-              let info3Lpln = mylogCpdlc.match(/RECEPTION MSG CPC|ENVOI MSG CPC|TRFDL|FPCRD   EVT TRFSEC|TRARTV|VTR  SECTEUR|EVENEMENT DATE: FIN VOL|FPCLOSE EVT END/);
+              let info3Lpln = mylogCpdlc.match(/RECEPTION MSG CPC|ENVOI MSG CPC|TRFDL|FPCRD   EVT TRFSEC|FPCRD   EVT ETATDL|TRARTV|VTR  SECTEUR|EVENEMENT DATE: FIN VOL|FPCLOSE EVT END/);
               if (info3Lpln !== null) {
                 fs.writeSync(w, mylogCpdlc + "\n", null, 'utf8');
                 //console.log(mylogCpdlc);
