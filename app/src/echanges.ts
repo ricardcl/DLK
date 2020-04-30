67f4bfef727f0b29db7d16cde32df01d67f829c0
Unknown <clarika.91@gmail.com>
2020-04-30 16:30:22 +0200

    Ajout de logs dans le fichier echanges.ts

--- app/src/echanges.ts
+++ app/src/echanges.ts
@@ -1,5 +1,3 @@
-var SocketIOFileUpload = require("socketio-file-upload");
-
 import { Contexte } from './Modele/enumContexte';
 import { checkAnswer } from './Modele/interfacesControles';
 import { Vol } from './Modele/vol';
@@ -17,6 +15,8 @@ import { LogBook } from './logBook';
 import { Database } from './database';
 import { Identifiants, inputData } from './Modele/identifiants';
 import { Split } from './Parseur/split';
+var SocketIOFileUpload = require("socketio-file-upload");
+
 //import { Ftp } from './ftp';
 
 
@@ -55,27 +55,13 @@ export class Echanges {
         this.controles = new Controles(this.dates);
         this.conception = new Conception(this.dates, this.frequences);
 
+        //Gestion des logs
         this.logBook = LogBook.getInstance();
-
-
         //Traitement bdd
         this.database = new Database();
-        /**
-        this.database.query('SELECT * from vol')
-            .then(rows => {
-                console.log(" database connectee", rows);
-            })
-            .catch(err => {
-                // handle the error
-                console.log("bdd non connectee", err);
-
-            });*/
-        //https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
-
-
         // Connexion FTP
-        //  this.clientFtp = new Ftp();
-        //  this.clientFtp.connect();
+        /**  this.clientFtp = new Ftp();
+          this.clientFtp.connect(); **/
 
 
 
@@ -85,48 +71,56 @@ export class Echanges {
         this.io.on("connection", (socket) => {
             console.log('connexion d un client, ouverture d une socket pour la recuperation de fichier', socket.id);
 
+
+            /** ACTIONS LORS DE LA CONNEXION D UN CLIENT **/
+            //Creation d'un client 
             let clientId: string = socket.id;
             this.users.createUser(clientId);
+            //Creation d'une socket d'upload
+            let uploader = new SocketIOFileUpload();
+            uploader.dir = Path.userPath + "/" + clientId;
+            uploader.listen(socket);
+
+            //Declaration des parseurs et analyseurs de fichiers de log
             this.parseurVEMGSA = new ParseurVEMGSA(Path.userPath + "/" + clientId, this.dates);
             this.parseurLPLN = new ParseurLPLN(Path.userPath + "/" + clientId, this.dates, this.split);
             this.analyseLPLN = new AnalyseLPLN(this.parseurLPLN, this.dates, this.split, this.frequences);
             this.analyseVEMGSA = new AnalyseVEMGSA(this.parseurVEMGSA, this.dates, this.split, this.frequences);
-            let uploader = new SocketIOFileUpload();
-            uploader.dir = Path.userPath + "/" + clientId;
-            uploader.listen(socket);
+
+            //Traitement des Logs --> Sauvegarde du client connecté
             this.logBook.writeLogBook(clientId, "connexion client");
 
             // Connexion FTP
-            //   this.clientFtp = new Ftp();
-            //   this.clientFtp.connect2( socket, this.logBook,clientId );
+            /**   this.clientFtp = new Ftp();
+               this.clientFtp.connect2( socket, this.logBook,clientId );*/
 
-            /**  Traitement bdd : Récupération de la liste de tous les vols   */
+            //  Traitement bdd  --> Envoi au client de la liste des vols sauvegardés    
             this.database.readAllVol(socket);
 
+            /** FIN DES ACTIONS LORS DE LA CONNEXION D UN CLIENT **/
+
+
+            /** DEFINITION DES ACTIONS LORS DE LA DECONNEXION D UN CLIENT **/
             socket.on("disconnect", (socket) => {
                 console.log('disconnect', clientId);
                 this.logBook.writeLogBook(clientId, "deconnexion client");
                 this.users.deleteUser(clientId);
             });
 
+            /** DEFINITION DES ACTIONS LORS DU CHARGEMENT D UN FICHIER PAR LE CLIENT **/
             uploader.on("complete", (event) => {
                 console.log("upload complete", event.file.name);
                 let log: string = "telechargement fichier " + event.file.name;
                 this.logBook.writeLogBook(clientId, log);
             });
 
+            /** DEFINITION DES ACTIONS LORS DE L EVENEMENT 'analyseDataInput' COTE CLIENT **/
             socket.on('analyseDataInput', (arcid, plnid, lpln, listVemgsaInput) => {
-                // Create frequence files : 
+
+                // TODO : Create frequence files : 
                 this.frequences.GbdiToFreq(Path.STPVFilePath);  //A modifier de place !!! 
 
-                console.log("analyseDataInput");
-                console.log("analyseDataInput", "fileLpln", lpln);
-                console.log("typeof fileLpln", typeof lpln);
-                console.log("analyseDataInput", "fileVemgsa", listVemgsaInput);
-                console.log("typeof listVemgsaInput", typeof listVemgsaInput);
-                console.log("typeof listVemgsaInput.length", listVemgsaInput.length);
-                console.log("analyseDataInput", "arcid", arcid);
-                console.log("analyseDataInput", "plnid", plnid);
+                //Recuperation des dates des plages horaires des fichiers VEMGSA pour les traiter dans l'ordre chronologique
                 let plageVemgsa = new Array;
                 if (listVemgsaInput.length >= 2) {
                     plageVemgsa = this.parseurVEMGSA.orderVemgsa(listVemgsaInput);
@@ -135,59 +129,65 @@ export class Echanges {
                     plageVemgsa[0] = listVemgsaInput[0];
                 }
 
+                //Analyse du contexte , c'est à dire du type de fichiers en entrée ( LPLN, VEMGSA, LPLNVEMGSA)
                 this.contexte = this.controles.evaluationContexte(lpln, plageVemgsa);
+                //Vérification de la syntaxe des identifiants fournis par l'utilisateur et de leur présence dans les fichiers de logs
                 let resultCheck = <checkAnswer>{};
                 resultCheck = this.controles.controle(arcid, plnid, lpln, plageVemgsa, this.contexte, this.parseurLPLN, this.parseurVEMGSA);
+                //Envoi du résultat du check à l'utilisateur 
+
                 socket.emit("check", resultCheck);
 
             });
 
+            /** DEFINITION DES ACTIONS LORS DE L EVENEMENT 'analysing' COTE CLIENT **/
             socket.on('analysing', (id: Identifiants, lplnfilename, vemgsafilename, checkanswer: checkAnswer) => {
+                console.log("Evenement --> analysing");
+
+                //Traitement des Logs --> Sauvegarde de la demande d'analyse
                 this.logBook.writeLogBook(clientId, "analysing " + "arcid: " + id.arcid + "plnid: " + id.plnid + "lplnfilename: " + lplnfilename + "vemgsafilename: " + vemgsafilename);
-                console.log("analysing données recues: " + "arcid " + id.arcid, "plnid: ", id.plnid, 'lplnfilename : ', lplnfilename, 'vemgsafilename : ', vemgsafilename, 'checkanswer : ', checkanswer);
-                console.log("analysing données recues: ", "creneauHoraire", id.dates);
-                console.log("analysing données recues: ", "inLpln", id.inLpln, "inVemgsa", id.inVemgsa);
 
-                console.log("this.contexte av", this.contexte);
-
+                //Recuperation des données du vol à analyser ( id complet, nom des fichiers de log)
                 let inputData: inputData = <inputData>{};
                 inputData.identifiant = id;
                 inputData.lplnfilename = lplnfilename;
                 inputData.vemgsafilename = vemgsafilename;
 
+                //mise à jour du type de contexte (LPLN, VEMGSA, LPLNVEMGSA ) en cas de données manquantes
                 if ((this.contexte == Contexte.LPLNVEMGSA) && (!id.inLpln)) {
                     this.contexte = Contexte.VEMGSA;
                 }
                 if ((this.contexte == Contexte.LPLNVEMGSA) && (!id.inVemgsa)) {
                     this.contexte = Contexte.LPLN;
                 }
-                console.log("this.contexte ap", this.contexte);
 
-                console.log("analysedVol");
+
+                //Création des vols en fonction du type de données en entrée
                 let volLpln, volVemgsa, volMix;
                 switch (this.contexte) {
-                    case Contexte.LPLN:
 
+                    case Contexte.LPLN:
                         console.log("analysedVol Contexte.LPLN");
                         volLpln = this.conception.InfosLpln(id.arcid, id.plnid, lplnfilename, this.analyseLPLN);
+                        //Envoi des données calculées à l'utilisateur 
                         socket.emit("analysedVol", "LPLN", inputData, volLpln, null, null);
-
-                        /**  Traitement bdd */
-                        // Mise à jour de la  bdd avec le vol analysé
-                        this.database.writeVol(id, "LPLN",inputData, volLpln, null, null);
-                        //  Traitement bdd : Envoi au client de la liste des vols actualisée 
+                        // Traitement bdd  --> Ajout du vol analysé dans la Bdd
+                        this.database.writeVol(id, "LPLN", inputData, volLpln, null, null);
+                        //  Traitement bdd  --> Envoi au client de la liste des vols actualisée 
                         this.database.readAllVol(socket);
-
                         break;
-                    case Contexte.VEMGSA:
-                        volVemgsa = this.conception.InfosVemgsa(id.arcid, id.plnid, id.dates, vemgsafilename, this.analyseVEMGSA);
 
+                    case Contexte.VEMGSA:
                         console.log("analysedVol Contexte.VEMGSA");
+                        volVemgsa = this.conception.InfosVemgsa(id.arcid, id.plnid, id.dates, vemgsafilename, this.analyseVEMGSA);
+                        //Envoi des données calculées à l'utilisateur 
                         socket.emit("analysedVol", "VEMGSA", inputData, null, volVemgsa, null);
-                        /**  Traitement bdd */
-                        this.database.writeVol(id, "VEMGSA",inputData, null, volVemgsa, null);
+                        // Traitement bdd  --> Ajout du vol analysé dans la Bdd
+                        this.database.writeVol(id, "VEMGSA", inputData, null, volVemgsa, null);
+                        //  Traitement bdd  --> Envoi au client de la liste des vols actualisée
                         this.database.readAllVol(socket);;
                         break;
+
                     case Contexte.LPLNVEMGSA:
                         console.log("analysedVol Contexte.LPLN et VEMGSA");
                         volLpln = this.conception.InfosLpln(id.arcid, id.plnid, lplnfilename, this.analyseLPLN);
@@ -195,9 +195,10 @@ export class Echanges {
                         volMix = this.conception.mixInfos(this.conception.InfosLpln(id.arcid, id.plnid, lplnfilename, this.analyseLPLN), this.conception.InfosVemgsa(id.arcid, id.plnid, id.dates, vemgsafilename, this.analyseVEMGSA), id.arcid, id.plnid, id.dates);
 
 
+                        //CAS 1/3 : des données sur le vol ont été identifiées dans les deux fichiers
                         if ((checkanswer.checkLPLN.valeurRetour <= 1) && (checkanswer.checkVEMGSA.valeurRetour <= 2)) {
                             console.log("analysedVol Contexte.LPLN et VEMGSA :  CAS 1 ");
-
+                            //Envoi des données calculées à l'utilisateur 
                             socket.emit("analysedVol", "MIX", inputData,
                                 // this.mixInfos.InfosLpln(id.arcid, id.plnid, lplnfilename, this.parseurLPLN),
                                 volLpln,
@@ -206,34 +207,36 @@ export class Echanges {
                                 //  this.mixInfos.mixInfos(this.mixInfos.InfosLpln(id.arcid, id.plnid, lplnfilename, this.parseurLPLN), this.mixInfos.InfosVemgsa(id.arcid, id.plnid, id.dates, vemgsafilename, this.parseurVEMGSA), id.arcid, id.plnid, id.dates)
                                 volMix
                             );
-                            /**  Traitement bdd */
-                            this.database.writeVol(id, "MIX",inputData, volLpln, volVemgsa, volMix);
+                            // Traitement bdd  --> Ajout du vol analysé dans la Bdd
+                            this.database.writeVol(id, "MIX", inputData, volLpln, volVemgsa, volMix);
+                            //  Traitement bdd  --> Envoi au client de la liste des vols actualisée
                             this.database.readAllVol(socket);
-
-
                         }
                         else {
+                            //CAS 2/3 : des données sur le vol ont été identifiées uniquement dans le ficher LPLN
                             if (checkanswer.checkLPLN.valeurRetour <= 1) {
-                                console.log("analysedVol Contexte.LPLN et VEMGSA :  CAS 2 ");
-
+                                //Envoi des données calculées à l'utilisateur 
                                 socket.emit("analysedVol", "LPLN", inputData, volLpln, null, null);
-                                this.database.writeVol(id, "LPLN",inputData, volLpln, null, null);
+                                // Traitement bdd  --> Ajout du vol analysé dans la Bdd
+                                this.database.writeVol(id, "LPLN", inputData, volLpln, null, null);
                             }
+
+                            //CAS 3/3 : des données sur le vol ont été identifiées uniquement dans le ficher VEMGSA
                             else {
-                                console.log("analysedVol Contexte.LPLN et VEMGSA :  CAS 3 ");
+                                //Envoi des données calculées à l'utilisateur 
                                 socket.emit("analysedVol", "VEMGSA", inputData, null, volVemgsa, null);
-                                this.database.writeVol(id, "LPLN",inputData, null, volVemgsa, null);
+                                // Traitement bdd  --> Ajout du vol analysé dans la Bdd
+                                this.database.writeVol(id, "LPLN", inputData, null, volVemgsa, null);
                             }
                         }
-
                         break;
-
                 }
 
             });
 
+            /** DEFINITION DES ACTIONS LORS DE L EVENEMENT 'getVolFromDatabase' COTE CLIENT **/
             socket.on("getVolFromDatabase", (id: string) => {
-                console.log("getVolFromDatabase", id);
+                console.log("Evenement --> getVolFromDatabase");
                 this.database.readVol(socket, id)
             });
 
