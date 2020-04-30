44be55f9c5f144b38f77d6cd4e0ee3ed139a7625
Unknown <clarika.91@gmail.com>
2020-04-30 16:38:07 +0200

    AJout de logs dans le fichier logbook.ts

--- app/src/logBook.ts
+++ app/src/logBook.ts
@@ -1,33 +1,43 @@
-const p = require('path');
-const fs = require('fs');
 import * as moment from 'moment';
 import { Path } from './Modele/path';
+const p = require('path');
+const fs = require('fs');
 
-
+/**
+ * La Classe LogBook logge toutes les actions utilisateurs 
+ * 
+ */
 export class LogBook {
-    private static instance: LogBook = null;
+
     private fichierLog;
 
+    //Création d'une unique instance de gestion des logs
+    private static instance: LogBook = null;
     public static getInstance(): LogBook {
         if (LogBook.instance === null) {
             LogBook.instance = new LogBook();
         }
         return LogBook.instance;
     }
-
+    
+    /**
+     * Constructeur de la classe LogBook 
+     *  
+     * fichierLog : fichier dans lequel écrire les logs 
+     */
     private constructor() {
         console.log("Je rentre dans constructeur LogBook");
-
         this.fichierLog = fs.openSync(p.resolve(Path.logBookPath + "/logBook_" + moment().format("DD_MM_YYYY_hh_mm_s") + ".txt"), "a+");
-
-        //TO DO : creer singleton pour les frequences
-        //TO DO : afficher la date en pm et pas am
     }
-
+    /**
+     * Cette fonction écrit dans le fichier de log l'action associée à un utilisateur donné
+     * @param user utilisateur
+     * @param log action de l'utilisateur
+     */
     public writeLogBook(user: string, log: string): void {
         console.log("classe LogBook Fonction writeLogBook");
         fs.writeSync(this.fichierLog, String(moment().format()) + " user: " + user + " log: " + log + "\n", null, 'utf8');
     }
 
-     
+
 }
